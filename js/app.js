a9581ca479d36d985595970cd67203712e5aa603
Tim <tim.guenther@live.de>
2018-12-11 19:59:04 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var login_container = {
+  encrypted: true,
+  level: [
+    "content",
+    "content"
+  ]
+};
+
+var level = {
+  
+}
+
+function login(){
+  var login_password = $("#login_password");
+  if(true){
+    $("#login").hide();
+  }
+}
